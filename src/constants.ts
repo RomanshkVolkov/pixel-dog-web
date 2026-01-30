4a1c5665de41dedbd0e4e13c09b61ee3214a4561
Romanshk Volkov <jose@guz-studio.dev>
2026-01-30 01:47:48 -0600

    refactor: update api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080"; //'https://pixel-dog-posts.guz-studio.dev';
+export const API_URL = "https://pixel-dog-posts.guz-studio.dev";
 export const IMAGE_URL = "https://izeus20.blob.core.windows.net/pixel-dog";
 
 export const getImageUrl = (pathname: string): string => {
