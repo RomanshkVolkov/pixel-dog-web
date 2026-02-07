ac3f7af35e57f8645ab4ff0b9580311fafce1d18
Romanshk Volkov <jose@guz-studio.dev>
2026-02-06 22:59:43 -0600

    fix: temporal difference

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,7 @@ export const PAYMENT_API_URL = "http://localhost:8081";
 export const IMAGE_URL = "https://izeus20.blob.core.windows.net/pixel-dog";
 
 export const getImageUrl = (pathname: string): string => {
-  return `${IMAGE_URL}${pathname.replace(".jpg", ".webp")}`;
+  const isPost = pathname.startsWith("/posts/");
+  const renamePathname = isPost ? pathname : pathname.replace(".jpg", ".webp");
+  return `${IMAGE_URL}${renamePathname}`;
 };
