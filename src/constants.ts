b17ced8350f2a7117f537fe256c5302ecab1acef
Romanshk Volkov <jose@guz-studio.dev>
2026-02-16 16:21:20 -0600

    refactor: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const AUTH_API_URL = "http://localhost:8082"//  "https://pixel-dog.guz-studio.dev/api/auth-service";
+export const AUTH_API_URL = "https://pixel-dog.guz-studio.dev/api/auth-service";
 export const PAYMENT_API_URL =
-  "http://localhost:8081"; // "https://pixel-dog.guz-studio.dev/api/payment-service";
-export const POST_API_URL = "http://localhost:8080"; // "https://pixel-dog.guz-studio.dev/api/post-service";
+  "https://pixel-dog.guz-studio.dev/api/payment-service";
+export const POST_API_URL =  "https://pixel-dog.guz-studio.dev/api/post-service";
 export const IMAGE_URL = "https://izeus20.blob.core.windows.net/pixel-dog";
 
 export const getImageUrl = (pathname: string): string => {
